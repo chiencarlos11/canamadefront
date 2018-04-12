0d80df5d2891b1847653c3c7a598e9b48049a6c0
Juan Carlos Chang <chiencarlos11@gmail.com>
2018-04-11 20:56:43 -0400

    Card Content displaying values properly

---
+++ frontend/src/context/Constants.js
@@ -0,0 +1,3 @@
+export const LAURENT_ITEMS = ['date', 'po_number', 'original_width', 'original_height',
+                         'control_size', 'cassette_orientation', 'cassette_extra', 
+    'cassette_color', 'fabric_type', 'fabric_color', 'cassette_size', 'tube_tob', 'inner', 'outer','height'];
