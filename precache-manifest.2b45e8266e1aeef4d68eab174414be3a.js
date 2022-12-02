db29efc1e6f5ba1b611461eee521f72b35c1facd
Juan Carlos Chang <chiencarlos11@gmail.com>
2022-12-01 19:59:36 -0500

    Updates

---
+++ precache-manifest.2b45e8266e1aeef4d68eab174414be3a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "dba8ee539892b9fcaa90",
+    "url": "/canamadefront/static/css/main.96f7c145.chunk.css"
+  },
+  {
+    "revision": "dba8ee539892b9fcaa90",
+    "url": "/canamadefront/static/js/main.dba8ee53.chunk.js"
+  },
+  {
+    "revision": "a3bb4c490f53a449035a",
+    "url": "/canamadefront/static/css/1.5155e31f.chunk.css"
+  },
+  {
+    "revision": "a3bb4c490f53a449035a",
+    "url": "/canamadefront/static/js/1.a3bb4c49.chunk.js"
+  },
+  {
+    "revision": "7a780336dd36fc164aff",
+    "url": "/canamadefront/static/js/runtime~main.7a780336.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/canamadefront/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9327cc7911df3eb30c62cfeb893dde74",
+    "url": "/canamadefront/static/media/Laurent.9327cc79.png"
+  },
+  {
+    "revision": "ebb67d628ccb0f6065e442e101c58279",
+    "url": "/canamadefront/static/media/RollerShades.ebb67d62.png"
+  },
+  {
+    "revision": "0ab816f1b1047b06b6b3ae2b35b4a0bb",
+    "url": "/canamadefront/static/media/CanaMade.0ab816f1.png"
+  },
+  {
+    "revision": "50d24aaea140772bf9200298ecca0a82",
+    "url": "/canamadefront/static/media/VerticalBlinds.50d24aae.png"
+  },
+  {
+    "revision": "2263a49b53547edb67ec50d7107e3b20",
+    "url": "/canamadefront/static/media/CellularShades.2263a49b.png"
+  },
+  {
+    "revision": "2b319658e1c35bbc251c22c8a0360aa2",
+    "url": "/canamadefront/static/media/feathericons.2b319658.woff"
+  },
+  {
+    "revision": "3d65bed48662f481a94725cdbf49a370",
+    "url": "/canamadefront/static/media/feathericons.3d65bed4.eot"
+  },
+  {
+    "revision": "a417dc8613a03f96f5bf521d1183ccd2",
+    "url": "/canamadefront/static/media/feathericons.a417dc86.svg"
+  },
+  {
+    "revision": "c66815eb97646a0a04689c0ed0d6149b",
+    "url": "/canamadefront/static/media/feathericons.c66815eb.ttf"
+  },
+  {
+    "revision": "ad14b91688ac2a73b0315e8e1c9def43",
+    "url": "/canamadefront/index.html"
+  }
+];
